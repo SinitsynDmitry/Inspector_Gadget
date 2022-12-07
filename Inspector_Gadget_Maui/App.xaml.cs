fba9907a0ae737ce5d2fea1e8b89c7dc62cc2d27
Dmitry Sinitsyn <dimsin101@gmail.com>
2022-12-07 20:01:10 +0200

    New MAUI project added

---
+++ Inspector_Gadget_Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Inspector_Gadget_Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
