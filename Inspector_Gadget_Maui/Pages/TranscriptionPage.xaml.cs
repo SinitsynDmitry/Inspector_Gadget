a92244b43d7d87d2a250951abb1b50a185456539
Viktoria Zimmermann <viktoria@indicosys.com>
2022-12-08 23:14:22 +0200

    minor

--- Inspector_Gadget_Maui/Pages/TranscriptionPage.xaml.cs
+++ Inspector_Gadget_Maui/Pages/TranscriptionPage.xaml.cs
@@ -175,7 +175,7 @@ public partial class TranscriptionPage : ContentPage
 
                     await foreach (var token in api.Completions.StreamCompletionEnumerableAsync(new CompletionRequest("Extract keywords from this text:\n\n" + tbx_input.Text, temperature: 0.5, max_tokens: 60, top_p: 1.0, frequencyPenalty: 0.8, presencePenalty: 0)))
                     {
-                        tbx_keywords.Text += token.ToString();
+                        tbx_keywords.Text += token.ToString();                       
                     }
 
                 }
