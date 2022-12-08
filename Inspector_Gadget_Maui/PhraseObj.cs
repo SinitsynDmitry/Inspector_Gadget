32a312589498cbfd93a7adcca6986c659fedd8c4
Viktoria Zimmermann <viktoria@indicosys.com>
2022-12-08 23:20:36 +0200

    PhraseObj added (not ready) - for searching

---
+++ Inspector_Gadget_Maui/PhraseObj.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inspector_Gadget_Maui
+{
+    internal class PhraseObj
+    {
+    }
+}
