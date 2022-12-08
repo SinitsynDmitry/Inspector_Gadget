fc28bc9330d2337213d4ce10d15b8686f6c9c780
phjorgensen <phjorgensenn@gmail.com>
2022-12-08 15:45:22 +0100

    fix(SelectFilePage): fileselector on mac

--- Inspector_Gadget_Maui/Pages/SelectFilePage.xaml.cs
+++ Inspector_Gadget_Maui/Pages/SelectFilePage.xaml.cs
@@ -20,7 +20,7 @@ public partial class SelectFilePage : ContentPage
                 // { DevicePlatform.Android, new[] { "application/comics" } }, // MIME type
                 { DevicePlatform.WinUI, new[] { ".mp3", ".mp4" } }, // file extension
                 { DevicePlatform.Tizen, new[] { "*/*" } },
-                { DevicePlatform.macOS, new[] { "mp3", "mp4" } }, // UTType values
+                { DevicePlatform.MacCatalyst, new[] { "mp3", "mp4" } }, // UTType values
             });
 
             PickOptions options = new()
