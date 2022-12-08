47dc956828ba6941c5bfc1045ba48e4c800dfb42
Viktoria Zimmermann <viktoria@indicosys.com>
2022-12-08 09:22:56 +0200

    Settings broken

--- Inspector_Gadget_Maui/PublicExtensions.cs
+++ Inspector_Gadget_Maui/PublicExtensions.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Inspector_Gadget_Maui
 {
-    internal class PublicExtensions
+    public static class PublicExtensions
     {
+        public static string processModelEnums(this whisperModels model)
+        {
+            switch (model)
+            {
+                case whisperModels.tiny:
+                    return "tiny";                   
+                case whisperModels.base_:
+                    return "base";
+                case whisperModels.small:
+                    return "small";
+                case whisperModels.medium:
+                    return "medium";
+                case whisperModels.large:
+                    return "large";
+                case whisperModels.medium_en:
+                    return "medium.en";
+                default:
+                    return "tiny";
+            }
+        }
     }
 }
